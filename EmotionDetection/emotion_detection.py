f4a3165e047518199ff40ee9b474bde9f79b93e8
Thubelihle Sithole <swazimntimande@gmail.com>
2026-06-01 15:02:42 -0400

    Completed Task 4

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        url,
+        json=input_json,
+        headers=headers
+    )
+
+    response_dict = json.loads(response.text)
+
+    emotion = response_dict["emotionPredictions"][0]["emotion"]
+
+    anger = emotion["anger"]
+    disgust = emotion["disgust"]
+    fear = emotion["fear"]
+    joy = emotion["joy"]
+    sadness = emotion["sadness"]
+
+    dominant_emotion = max(emotion, key=emotion.get)
+
+    return {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
